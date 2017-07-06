b600c52f84943406069f01719f2311ff64f8b32c
dustjdxo <dustjdxo@naver.com>
2017-07-06 21:57:42 +0900

    별찍기

---
+++ ex01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 2;
+		for (int j = 1; j < 10; ++j) 
+
+			printf("%d x %d = %d\n", i, j, i*j);
+	
+}
---
+++ ex02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 5; i > 0 ; --i)
+	{
+		for (int j = 0; j < 5 - i; ++j)
+		{
+			printf(" ");
+		}
+
+		for (int j = 0; j < i; ++j)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+}
