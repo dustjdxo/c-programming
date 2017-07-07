33e3474c558d2f6c068f3d6668897275f564c6ea
dustjdxo <dustjdxo@naver.com>
2017-07-07 20:50:23 +0900

    별찍기

---
+++ ex03.c
@@ -0,0 +1,15 @@
+// º°Âï±â
+
+#include <stdio.h>
+
+int main()
+{
+	for (int i = 0; i < 5; ++i) {
+		for (int j = 0; j < 4 - i; ++j)
+			printf(" ");
+
+		for (int k = 0; k < i + 1; ++k)
+			printf("*");
+		printf("\n");
+	}
+}
